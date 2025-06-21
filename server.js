609dd00d463c0d86a9392b942073c0e9025fdf33
Reidner S.M. <reidner.medeiros@academico.unirv.edu.br>
2025-06-21 02:27:43 -0300

    Alteração em pastas

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 3000;
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'view')));
 
 // Rotas
 app.use('/api/auth', authRoutes);
@@ -34,7 +34,7 @@ app.use('/api/dashboard', dashboardController);
 
 // Rota para a tela inicial
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'view', 'login.html'));
 });
 
 // Inicialização do servidor
